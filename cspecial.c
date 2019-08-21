b4ddc958b4bffda205d59fbbc24a836365eb0f09
alzheimeer <fogniebla@hotmail.com>
2019-08-21 10:39:31 -0500

    feature cspecial

---
+++ cspecial.c
@@ -0,0 +1,40 @@
+#include "shell.h"
+/**
+ * special$$ - search $ in the command
+ * @line2: command
+ * Return: 1 is found, else 0
+ */
+int special$$(char line2[])
+{
+	if (_strchr(line2, '$')!=NULL)
+	{
+		return 1;
+	}
+	return 0;
+}
+/**
+ * comment - search # in the command
+ * @line2: command
+ * Return: 1 is found, else 0
+ */
+int comment(char line2[])
+{
+	if (_strchr(line2, '#')!=NULL)
+	{
+		return 1;
+	}
+	return 0;
+}
+/**
+ * Space - search spaces in the command
+ * @line2: command
+ * Return: 1 is found, else 0
+ */
+int Space(char line2[])
+{
+	if (_strchr(line2, ' ')!=NULL)
+	{
+		return 1;
+	}
+	return 0;
+}
